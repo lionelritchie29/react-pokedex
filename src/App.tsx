a03448b0ee6e0dbb81d0f9fa343070e2f9db2254
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-07-18 12:21:24 +0700

    Add emotion, tailwind, twin macro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import { jsx } from '@emotion/react'
+
 import React from 'react';
 import './App.css';
 import {useQuery} from '@apollo/client'
 import { GET_ALL_POKEMONS } from './queries/queries'
 import Pokemon from './models/pokemon'
+import tw from 'twin.macro'
+
+
+const TestHeading = tw.h1`text-blue-500 text-xl hover:text-red-500`
 
 function App() {
   const {loading, error, data} = useQuery(GET_ALL_POKEMONS)
@@ -17,7 +25,7 @@ function App() {
     <div className="App">
       {pokemons.map(pokemon => {
         return (
-          <div key={pokemon.id}>{pokemon.name}</div>
+          <TestHeading key={pokemon.id}>{pokemon.name}</TestHeading>
         )
       })}
     </div>
